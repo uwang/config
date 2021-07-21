d7e08adb982255d5c1afb98e1f3362d9fa831f18
u.wang <9262286+uwang@users.noreply.github.com>
2021-07-21 16:53:00 +0800

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 平常模式使用单引号
+  singleQuote: true,
+  // 尾随逗号
+  trailingComma: 'none',
+  // tab 为2个空格长度
+  tabWidth: 2,
+  // 不需要分号
+  semi: false,
+  // 单行长度
+  printWidth: 120
+}
